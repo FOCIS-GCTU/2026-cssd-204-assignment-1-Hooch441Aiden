6ad0771a8d3d049eee2317421bf91305f07eafea
Hooch441Aiden <136399093+Hooch441Aiden@users.noreply.github.com>
2026-05-31 08:25:27 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,22 @@
-
 def main():
   pass #code goes here
 
+    print()
+    print("...TYF")
+    print()
+
+    print("...TTTTTTTTTTTT...YYYY....YYYY...FFFFFFFFFFFF...")
+    print("......TTT........YYYY....YYYY...FFF............")
+    print("......TTT.........YYYY..YYYY....FFF............")
+    print("......TTT..........YYYYYYYY.....FFFFFFFFFF.....")
+    print("......TTT...........YYYYYY......FFF............")
+    print("......TTT............YYYY.......FFF............")
+    print("......TTT............YYYY.......FFF............")
+    print("......TTT............YYYY.......FFF............")
+    print("...**.TTT.........**.YYYY....**.FFF.........**.")
+    print("...**.TTT.........**.YYYY....**.FFF.........**.")
+
+    print()
 
 
 main()
